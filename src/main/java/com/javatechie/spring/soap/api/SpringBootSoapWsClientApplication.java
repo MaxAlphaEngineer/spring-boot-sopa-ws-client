b6041c3c68d807eb5b1d14a67c70c8626d3bccfe
muzaffar.makhkamov <maxalphaengineer@gmail.com>
2021-01-06 18:07:55 +0500

    Naming fixed

--- src/main/java/com/javatechie/spring/soap/api/SpringBootSoapWsClientApplication.java
+++ src/main/java/com/javatechie/spring/soap/api/SpringBootSoapWsClientApplication.java
@@ -7,24 +7,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javatechie.spring.soap.api.clinet.SoapClinet;
+import com.javatechie.spring.soap.api.clinet.SoapClient;
 import com.javatechie.spring.soap.api.loaneligibility.Acknowledgement;
 import com.javatechie.spring.soap.api.loaneligibility.CustomerRequest;
 
 @SpringBootApplication
 @RestController
-public class SpringBootSopaWsClientApplication {
+public class SpringBootSoapWsClientApplication {
 	
 	@Autowired
-	private SoapClinet clinet;
+	private SoapClient client;
 	
 	@PostMapping("/getLoanStatus")
 	public Acknowledgement invokeSoapClientToGetLoanStatus(@RequestBody CustomerRequest request) {
-		return clinet.getLoanStatus(request);
+		System.out.println("Rest called   ");
+		return client.getLoanStatus(request);
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSopaWsClientApplication.class, args);
+		SpringApplication.run(SpringBootSoapWsClientApplication.class, args);
 	}
 
 }
--- src/test/java/com/javatechie/spring/soap/api/SpringBootSoapWsClientApplicationTests.java
+++ src/test/java/com/javatechie/spring/soap/api/SpringBootSoapWsClientApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootSopaWsClientApplicationTests {
+public class SpringBootSoapWsClientApplicationTests {
 
 	@Test
 	public void contextLoads() {
